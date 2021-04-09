d4c768b1df1dbdb843b00cf32f2a6dc8a41be51b
Turtle <hypoturtle+gh@gmail.com>
2021-04-09 17:34:26 +0100

    Exploding duck

--- aaaaAAAA/game.py
+++ aaaaAAAA/game.py
@@ -347,10 +347,13 @@ class DuckScene(BaseScene):
             self.alter_toxicity(+1)
             self.streak = 0
 
-    def explode(self, ducky: arcade.Sprite) -> None:
+    def explode(self, ducky: _sprites.Ducky) -> None:
         """Blow up a denied duck."""
         # Super impressive explosions
-        ducky.width = 0
+        ducky.alpha = 190
+        seq = ducky.explode()
+        seq.add_callback(seq.total_time, lambda: self.ducks.remove(ducky))
+        self.animations.fire(ducky, seq)
 
     def move_to_path_queue(self, ducky: _sprites.Ducky) -> None:
         """Move the ducky into the path_queue spritelist."""
